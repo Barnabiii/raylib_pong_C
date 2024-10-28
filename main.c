7c7d329b4ef0e4e8a50a761dc78d77d73eee5002
Barnabiii <86511805+Barnabiii@users.noreply.github.com>
2024-10-28 20:13:14 +0100

    fix movement + add player 2

--- main.c
+++ main.c
@@ -1,60 +1,87 @@
 #include <stdio.h>
 #include "raylib.h"
 
-#define UP 265
-#define DOWN 264
+#define UP_ARROW 265
+#define DOWN_ARROW 264
+#define W 87
+#define S 83
 
+const int SCREEN_WIDTH = 1000;
+const int SCREEN_HEIGHT = 600;
 const int PLAYER_SPEED = 300;
+const int PLAYER_WIDTH = 10;
+const int PLAYER_HEIGHT = 100;
+const int x_offset = 20;
 
 float deltaTime;
 
 struct Player
 {
-    int posX;
-    int posY;
+    float posX;
+    float posY;
     int width;
     int height;
+    int up_key;
+    int down_key;
 };
 
-
-void move_player(struct Player *pPlayer);
-void draw_player(struct Player *pPlayer);
-
-int main()
-{
-    const int screenWidth = 1000;
-    const int screenHeight = 600;
-
-    InitWindow(screenWidth, screenHeight, "raycaster");
-    SetTargetFPS(60);
-
-
-    struct Player player1;
-    player1.posX = 20;
-    player1.posY = 250;
-    player1.width = 10;
-    player1.height = 100;
-
-    while (!WindowShouldClose()) {  // Detect window close button or ESC key
-        deltaTime = GetFrameTime();
-
-        move_player(&player1);
-        BeginDrawing();
-        ClearBackground(BLACK);
-        draw_player(&player1);
-        EndDrawing();
-    }
-
-    CloseWindow();
-    return 0;
+void init_player(struct Player *pPlayer,int init_posX,int up_key,int down_key) {
+    pPlayer->posX = init_posX;
+    pPlayer->posY = 250;
+    pPlayer->width = PLAYER_WIDTH;
+    pPlayer->height = PLAYER_HEIGHT;
+    pPlayer->up_key = up_key;
+    pPlayer->down_key = down_key;
+}
+void init_players(struct Player *pPlayer1,struct Player *pPlayer2) {
+    int player2_startX = SCREEN_WIDTH - (x_offset+PLAYER_WIDTH);   
+    init_player(pPlayer1,x_offset,W,S);
+    init_player(pPlayer2,player2_startX,UP_ARROW,DOWN_ARROW);
 }
 
 void move_player(struct Player *pPlayer) {
-    short dir = IsKeyDown(DOWN) - IsKeyDown(UP);
+    short dir = IsKeyDown(pPlayer->down_key) - IsKeyDown(pPlayer->up_key);
     pPlayer->posY += dir * PLAYER_SPEED * deltaTime;
 }
 
 void draw_player(struct Player *pPlayer) {
     struct Player player = *pPlayer;
-    DrawRectangle(player.posX,player.posY,player.width,player.height,WHITE);
+    DrawRectangle((int)player.posX,(int)player.posY,player.width,player.height,WHITE);
+}
+void draw(struct Player *pPlayer1,struct Player *pPlayer2) {
+    BeginDrawing();
+
+    ClearBackground(BLACK);  
+
+    draw_player(pPlayer1);
+    draw_player(pPlayer2);
+
+    EndDrawing();
+}
+
+void process_game(struct Player *pPlayer1,struct Player *pPlayer2) {
+    while (!WindowShouldClose()) {  // Detect window close button or ESC key
+        deltaTime = GetFrameTime();
+        // int k = GetKeyPressed();
+        // if(k != 0) {
+        //     printf("key:\t%d,\t%c\n",k,k);
+        // }
+
+        move_player(pPlayer1);
+        move_player(pPlayer2);
+        draw(pPlayer1,pPlayer2);
+    }
+}
+
+int main(int argc, char** argv) {
+
+    InitWindow(SCREEN_WIDTH,SCREEN_HEIGHT, "PONG");
+    SetTargetFPS(200);
+
+    struct Player player1,player2;
+    init_players(&player1,&player2);
+    process_game(&player1,&player2);
+
+    CloseWindow();
+    return 0;
 }
