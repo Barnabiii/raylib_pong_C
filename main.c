5aec6fec9ab825ab9017ec5cb1fb0a4f657f6bd2
Barnabiii <86511805+Barnabiii@users.noreply.github.com>
2024-10-29 20:15:54 +0100

    general game management update

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <conio.h>
 #include "raylib.h"
 
 #define UP_ARROW 265
@@ -10,37 +11,37 @@ const int SCREEN_WIDTH = 1000;
 const int SCREEN_HEIGHT = 600;
 const int SCREEN_BORDERS = 100;
 
-const int PLAYER_SPEED = 300;
+const int PLAYER_SPEED = 200;
 const int PLAYER_WIDTH = 20;
 const int PLAYER_HEIGHT = 100;
 
-const int BALL_SPEED = 400;
+const int BALL_SPEED = 300;
 const int BALL_SIZE = 20;
 
-float deltaTime;
+const int FRICTION = 0.95;
 
-struct Body
+float deltaTime;
+float accelerating_speed;
+int gameLost;
+int score;
+
+typedef struct
 {
     float posX;
     float posY;
+    float velX;
+    float velY;
     int width;
     int height;
     Color color;    
-};
+} Body;
 
-struct Ball
+typedef struct
 {
-    struct Body body;
-    float velX;
-    float velY;
-};
-
-struct Player
-{
-    struct Body body;
+    Body body;
     int up_key;
     int down_key;
-};
+} Player;
 
 float clamp(float a,float min, float max) {
     a = a < min ? min : a;
@@ -56,7 +57,7 @@ int getCollisionMag(int pos1, int size1,int pos2,int size2) {
     return -collision_on_axis;
 }
 
-void define_body(struct Body *pBody,int posX,int posY,int width,int height) {
+void define_body(Body *pBody,int posX,int posY,int width,int height) {
     pBody->posX = (float)posX;
     pBody->posY = (float)posY;
     pBody->width = width;
@@ -64,22 +65,22 @@ void define_body(struct Body *pBody,int posX,int posY,int width,int height) {
     pBody->color = WHITE;
 }
 
-void define_player(struct Player *pPlayer,int init_x,int up_key,int down_key) {
+void define_player(Player *pPlayer,int init_x,int up_key,int down_key) {
     define_body(&pPlayer->body,init_x,250,PLAYER_WIDTH,PLAYER_HEIGHT);
     pPlayer->up_key = up_key;
     pPlayer->down_key = down_key;
 }
 
-void move_ball(struct Ball *pBall,struct Player *pPlayer1,struct Player *pPlayer2) {
-    float x = pBall->body.posX;
-    float y = pBall->body.posY;
-    x += pBall->velX * BALL_SPEED * deltaTime;
-    y += pBall->velY * BALL_SPEED * deltaTime;
+void move_ball(Body *pBall,Player *pPlayer1,Player *pPlayer2) {
+    float x = pBall->posX;
+    float y = pBall->posY;
+    x += pBall->velX * (BALL_SPEED+accelerating_speed) * deltaTime;
+    y += pBall->velY * (BALL_SPEED+accelerating_speed) * deltaTime;
 
     float clamped_x = clamp(x,0,SCREEN_WIDTH-BALL_SIZE);
     float clamped_y = clamp(y,0,SCREEN_HEIGHT-BALL_SIZE);
-
-    pBall->velX = clamped_x != x ? -pBall->velX : pBall->velX;
+    
+    gameLost = clamped_x != x;
     pBall->velY = clamped_y != y ? -pBall->velY : pBall->velY;
 
     int xCol1 = getCollisionMag( (int) clamped_x , BALL_SIZE  ,  (int) pPlayer1->body.posX , PLAYER_WIDTH );
@@ -88,46 +89,68 @@ void move_ball(struct Ball *pBall,struct Player *pPlayer1,struct Player *pPlayer
     int yCol2 = getCollisionMag( (int) clamped_y , BALL_SIZE  ,  (int) pPlayer2->body.posY , PLAYER_HEIGHT);
 
     if((xCol1 >= 0 && yCol1 >= 0)) {
-        if(xCol1 <= yCol1) {pBall->velX *= -1;} 
-        if(xCol1 >= yCol1) {pBall->velY *= -1;}
+        if(xCol1 <= yCol1) {
+            pBall->velX *= -1;
+            pBall->velY += pPlayer1->body.velY * FRICTION;
+            score +=1;
+        } 
+
+        if(xCol1 >= yCol1) {
+            pBall->velY += pPlayer1->body.velY;
+            if ((pBall->velY * pPlayer1->body.velY) <= 0) {pBall->velY *= -1;}
+        }
     }
 
     if((xCol2 >= 0 && yCol2 >= 0)) {
-        if(xCol2 <= yCol2) {pBall->velX *= -1;} 
-        if(xCol2 >= yCol2) {pBall->velY *= -1;}
+        if(xCol2 <= yCol2) {
+            pBall->velY += pPlayer2->body.velY * FRICTION;
+            pBall->velX *= -1;
+            score +=1;
+        } 
+        if(xCol2 >= yCol2) {
+            pBall->velY += pPlayer2->body.velY;
+            if ((pBall->velY * pPlayer2->body.velY) <= 0) {pBall->velY *= -1;}
+        }
     }
 
-    pBall->body.posX = clamped_x;
-    pBall->body.posY = clamped_y;
+    pBall->posX = clamped_x;
+    pBall->posY = clamped_y;
 }
 
-void move_player(struct Player *pPlayer) {
+void move_player(Player *pPlayer) {
     short dir = IsKeyDown(pPlayer->down_key) - IsKeyDown(pPlayer->up_key);
 
+    pPlayer->body.velY = dir * (PLAYER_SPEED+accelerating_speed) * deltaTime;
+
     float y = pPlayer->body.posY;
-    y += dir * PLAYER_SPEED * deltaTime;
+
+    y += pPlayer->body.velY;
     pPlayer->body.posY = clamp(y,0,SCREEN_HEIGHT-PLAYER_HEIGHT);
 }
 
-void draw_body(struct Body *pBody) {
+void draw_body(Body *pBody) {
     DrawRectangle(pBody->posX,pBody->posY,pBody->width,pBody->height,pBody->color);
 }
 
-void draw(struct Player *pPlayer1,struct Player *pPlayer2,struct Ball *pBall) {
+void draw(Player *pPlayer1,Player *pPlayer2,Body *pBall,int score) {
     BeginDrawing();
 
     ClearBackground(BLACK);  
 
+    DrawText(TextFormat("%d",score), 480, 20, 50, GRAY);
     draw_body(&pPlayer1->body);
     draw_body(&pPlayer2->body);
-    draw_body(&pBall->body);
+    draw_body(pBall);
 
     EndDrawing();
 }
 
-void process_game(struct Player *pPlayer1,struct Player *pPlayer2,struct Ball *pBall) {
-    int gameLost = 0;
-    while (!WindowShouldClose() || !gameLost) {  // Detect window close button or ESC key
+void process_game(Player *pPlayer1,Player *pPlayer2,Body *pBall) {
+    gameLost = 0;
+    score = 0;
+    printf("hello");
+    while (!WindowShouldClose() && !gameLost) {  // Detect window close button or ESC key
+        
         deltaTime = GetFrameTime();
         // int k = GetKeyPressed();
         // if(k != 0) {
@@ -137,8 +160,36 @@ void process_game(struct Player *pPlayer1,struct Player *pPlayer2,struct Ball *p
         move_player(pPlayer1);
         move_player(pPlayer2);
         move_ball(pBall,pPlayer1,pPlayer2);
+        draw(pPlayer1,pPlayer2,pBall,score);
 
-        draw(pPlayer1,pPlayer2,pBall);
+        accelerating_speed += deltaTime * 5;
+    }
+}
+
+void init_game() {
+    while (!WindowShouldClose()) {
+        Player player1, player2;
+        define_player(&player1, SCREEN_BORDERS, W, S);
+        define_player(&player2, SCREEN_WIDTH - PLAYER_WIDTH - SCREEN_BORDERS, UP_ARROW, DOWN_ARROW);
+
+        Body ball;
+        define_body(&ball, 490, 290, BALL_SIZE, BALL_SIZE);
+        ball.velX = 1;
+        ball.velY = 0.5;
+
+        int k = GetKeyPressed();
+        if (k != 0) {
+            process_game(&player1, &player2, &ball);
+        }
+            
+        BeginDrawing();
+        ClearBackground(BLACK);
+        draw_body(&player1.body);
+        draw_body(&player2.body);
+        draw_body(&ball);
+        DrawText(TextFormat("Press any key to start the round"), 300, 280, 20, RED);
+        EndDrawing();
+        
     }
 }
 
@@ -146,17 +197,7 @@ int main(int argc, char** argv) {
 
     InitWindow(SCREEN_WIDTH,SCREEN_HEIGHT, "PONG");
     SetTargetFPS(200);
-
-    struct Player player1,player2;
-    define_player(&player1,SCREEN_BORDERS,W,S);
-    define_player(&player2,SCREEN_WIDTH-PLAYER_WIDTH-SCREEN_BORDERS,UP_ARROW,DOWN_ARROW);
-
-    struct Ball ball;
-    define_body(&ball.body,490,290,BALL_SIZE,BALL_SIZE);
-    ball.velX = 1;
-    ball.velY = 0.5;
-
-    process_game(&player1,&player2,&ball);
+    init_game();
 
     CloseWindow();
     return 0;
